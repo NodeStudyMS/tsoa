d48f93e0d8060c90c53e5bdc1da44e160bb829b0
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-05-13 14:25:55 +0900

    Update db.ts

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -11,9 +11,11 @@ const pool = mariadb.createPool({
 });
 
 export default {
+  // DB 연결 직접 가져오는 함수
   getConnection: async () => {
     return await pool.getConnection();
   },
+  // Query 실행 함수 sql : 실행할 SQL 문장, params : SQL에 전달할 값들
   query: async (sql: string, params?: any[]) => {
     let conn;
     try {
