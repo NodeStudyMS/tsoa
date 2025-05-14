11bbd7ac499a8bd4f7eae648a3210fd936f22d92
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-05-14 09:54:14 +0900

    Sequelize 이관
    기존 dto 방식에서 sequelize 사용 위해 인터페이스 추가

--- backend/src/models/Member.ts
+++ backend/src/models/Member.ts
@@ -1,22 +1,60 @@
 // src/models/Member.ts
-export interface Member {
-  MID: string;  
-  MPW: string;  
+import { Model, DataTypes, Sequelize } from 'sequelize';
+
+export interface MemberAttributes {
+  MID: string;
+  MPW: string;
   MEMBERNAME: string | null;
 }
 
 export interface MemberCreationParams {
-  MID: string;  
-  MPW: string;  
+  MID: string;
+  MPW: string;
   MEMBERNAME?: string;
 }
 
 export interface MemberLoginParams {
-  MID: string;  
-  MPW: string;  
+  MID: string;
+  MPW: string;
 }
 
 export interface MemberUpdateParams {
-  MPW?: string;  
+  MPW?: string;
   MEMBERNAME?: string;
+}
+
+// Sequelize
+export class Member extends Model<MemberAttributes, MemberCreationParams> implements MemberAttributes {
+  public MID!: string;
+  public MPW!: string;
+  public MEMBERNAME!: string | null;
+  
+  // timestamp field
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // 모델 초기화 메서드
+  static initModel(sequelize: Sequelize): typeof Member {
+    Member.init({
+      MID: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false
+      },
+      MPW: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      MEMBERNAME: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    }, {
+      sequelize,
+      tableName: 'member',
+      timestamps: true // createdAt, updatedAt 자동 생성
+    });
+    
+    return Member;
+  }
 }
